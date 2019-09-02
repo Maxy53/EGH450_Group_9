0c302df05765db01f1c19f2dece00f6b5007a9ff
Maxy53 <54821949+Maxy53@users.noreply.github.com>
2019-09-03 09:53:46 +1000

    Update image_processor.py

--- src/egh450_image_processor/image_processor.py
+++ src/egh450_image_processor/image_processor.py
@@ -6,6 +6,9 @@ import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CameraInfo
 
 class ImageProcessor():
 	def __init__(self):
@@ -17,20 +20,69 @@ class ImageProcessor():
 		# Set up the CV Bridge
 		self.bridge = CvBridge()
 
-		# Set up the subscriber
-		self.sub_img = rospy.Subscriber('~input/image_raw/compressed', CompressedImage, self.callback_img)
-		self.pub_img = rospy.Publisher('~output/image_raw/compressed', CompressedImage, queue_size=1)
+		# Load in parameters from ROS
+		self.param_use_compressed = rospy.get_param("~use_compressed", False)
+		self.param_circle_radius = rospy.get_param("~circle_radius", 1.0)
+		self.param_hue_center = rospy.get_param("~hue_center", 170)
+		self.param_hue_range = rospy.get_param("~hue_range", 20) / 2
+		self.param_sat_min = rospy.get_param("~sat_min", 50)
+		self.param_sat_max = rospy.get_param("~sat_max", 255)
+		self.param_val_min = rospy.get_param("~val_min", 50)
+		self.param_val_max = rospy.get_param("~val_max", 255)
+
+		# Set additional camera parameters
+		self.got_camera_info = False
+		self.camera_matrix = None
+		self.dist_coeffs = None
+
+		# Set up the publishers, subscribers, and tf2
+		self.sub_info = rospy.Subscriber("~camera_info", CameraInfo, self.callback_info)
+
+		if self.param_use_compressed:
+			self.sub_img = rospy.Subscriber("~image/compressed", CompressedImage, self.callback_img)
+			self.pub_mask = rospy.Publisher("~debug/image/compressed", CompressedImage, queue_size=1)
+			self.pub_overlay = rospy.Publisher("~overlay/image/compressed", CompressedImage, queue_size=1)
+			self.pub_img = rospy.Publisher("~image/compressed")
+		else:
+			self.sub_img = rospy.Subscriber("~image_raw", Image, self.callback_img)
+			self.pub_mask = rospy.Publisher("~debug/image_raw", Image, queue_size=1)
+			self.pub_overlay = rospy.Publisher("~overlay/image_raw", Image, queue_size=1)
+
+		self.tfbr = tf2_ros.TransformBroadcaster()
 
 	def shutdown(self):
 		# Unregister anything that needs it here
 		self.sub_img.unregister()
+		self.sub_info.unregister()
+
+	# Collect in the camera characteristics
+	def callback_info(self, msg_in):
+		self.dist_coeffs = np.array([[msg_in.D[0], msg_in.D[1], msg_in.D[2], msg_in.D[3], msg_in.D[4]]], dtype="double")
+
+		self.camera_matrix = np.array([
+                 (msg_in.P[0], msg_in.P[1], msg_in.P[2]),
+                 (msg_in.P[4], msg_in.P[5], msg_in.P[6]),
+                 (msg_in.P[8], msg_in.P[9], msg_in.P[10])],
+				 dtype="double")
+
+		if not self.got_camera_info:
+			rospy.loginfo("Got camera info")
+			self.got_camera_info = True
 
 	def callback_img(self, msg_in):
-		# Convert ROS image to CV image
-		try:
-			cv_image = self.bridge.compressed_imgmsg_to_cv2( msg_in, "bgr8" )
-		except CvBridgeError as e:
-			print(e)
+		# Don't bother to process image if we don't have the camera calibration
+		if self.got_camera_info:
+			#Convert ROS image to CV image
+			cv_image = None
+
+			try:
+				if self.param_use_compressed:
+					cv_image = self.bridge.compressed_imgmsg_to_cv2( msg_in, "bgr8" )
+				else:
+					cv_image = self.bridge.imgmsg_to_cv2( msg_in, "bgr8" )
+			except CvBridgeError as e:
+				rospy.loginfo(e)
+				return
 
 		# ===================
 		# Do processing here!
