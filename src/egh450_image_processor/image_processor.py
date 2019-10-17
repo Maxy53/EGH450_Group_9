5c57b2d763bfc20eba538a1dc596a845fba07649
Maxy53 <54821949+Maxy53@users.noreply.github.com>
2019-10-17 10:02:32 +1000

    Add files via upload

--- src/egh450_image_processor/image_processor.py
+++ src/egh450_image_processor/image_processor.py
@@ -18,8 +18,11 @@ class ImageProcessor():
 		self.time_finished_processing = rospy.Time(0)
 		# Get the path to the cascade XML training file using ROS parameters
 		# Then load in our cascade classifier
-		sign_cascade_file = str(rospy.get_param("~cascade_file"))
-		self.sign_cascade = cv2.CascadeClassifier(sign_cascade_file)
+		sign_cascade_file_triangle = str(rospy.get_param("~cascade_file_triangle"))
+		self.sign_cascade_triangle = cv2.CascadeClassifier(sign_cascade_file_triangle)
+
+		sign_cascade_file_square = str(rospy.get_param("~cascade_file_square"))
+		self.sign_cascade_square = cv2.CascadeClassifier(sign_cascade_file_square)
 
 		# Set up the CV Bridge
 		self.bridge = CvBridge()
@@ -30,20 +33,21 @@ class ImageProcessor():
 		self.param_square_radius = rospy.get_param("~square_radius", 1.0)
 
 		# Orange Square
-		self.param_hue_center2 = rospy.get_param("~hue_center", 30)
-		self.param_hue_range2 = rospy.get_param("~hue_range", 60) / 2
-		self.param_sat_min2 = rospy.get_param("~sat_min", 100)
-		self.param_sat_max2 = rospy.get_param("~sat_max", 255)
-		self.param_val_min2 = rospy.get_param("~val_min", 100)
-		self.param_val_max2 = rospy.get_param("~val_max", 255)
+		self.param_hue_center_square = rospy.get_param("~hue_center_square", 30)
+		self.param_hue_range_square = rospy.get_param("~hue_range_square", 90) / 2
+		self.param_sat_min_square = rospy.get_param("~sat_min_square", 75)
+		self.param_sat_max_square = rospy.get_param("~sat_max_square", 255)
+		self.param_val_min_square = rospy.get_param("~val_min_square", 100)
+		self.param_val_max_square = rospy.get_param("~val_max_square", 255)
 
 		# Blue Triangle
-		self.param_hue_center = rospy.get_param("~hue_center2", 170)
-		self.param_hue_range = rospy.get_param("~hue_range2", 250) / 2
-		self.param_sat_min = rospy.get_param("~sat_min2", 50)
-		self.param_sat_max = rospy.get_param("~sat_max2", 255)
-		self.param_val_min = rospy.get_param("~val_min2", 50)
-		self.param_val_max = rospy.get_param("~val_max2", 255)
+		self.param_hue_center_triangle = rospy.get_param("~hue_center_triangle", 170)
+		self.param_hue_range_triangle = rospy.get_param("~hue_range_triangle", 250) / 2
+		self.param_sat_min_triangle = rospy.get_param("~sat_min_triangle", 50)
+		self.param_sat_max_triangle = rospy.get_param("~sat_max_triangle", 255)
+		self.param_val_min_triangle = rospy.get_param("~val_min_triangle", 50)
+		self.param_val_max_triangle = rospy.get_param("~val_max_triangle", 255)
+
 
 		# Set additional camera parameters
 		self.got_camera_info = False
@@ -68,12 +72,19 @@ class ImageProcessor():
 		# Generate the model for the pose solver
 		# For this example, draw a square around where the circle should be
 		# There are 5 points, one in the center, and one in each corner
-		r = self.param_triangle_radius
-		self.model_object = np.array([(0.0, 0.0, 0.0),
-										(r, r, 0.0),
-										(r, -r, 0.0),
-										(-r, r, 0.0),
-										(-r, -r, 0.0)])
+		r = self.param_square_radius
+		r2 = self.param_triangle_radius
+		self.model_object_square = np.array([(0.0, 0.0, 0.0),
+					      (r, r, 0.0),
+					      (r, -r, 0.0),
+					      (-r, r, 0.0),
+					      (-r, -r, 0.0)], dtype=np.float64)
+
+		self.model_object_triangle = np.array([(0.0, 0.0, 0.0),
+					      (r2, r2, 0.0),
+					      (r2, -r2, 0.0),
+					      (-r2, r2, 0.0),
+					      (-r2, -r2, 0.0)], dtype=np.float64)
 
 	def shutdown(self):
 		# Unregister anything that needs it here
@@ -98,8 +109,10 @@ class ImageProcessor():
 		if msg_in.header.stamp > self.time_finished_processing:
 			# Don't bother to process image if we don't have the camera calibration
 			cv_image = None	
-			self.model_image = None	
-			success = False;
+			self.model_image_square = None	
+			self.model_image_triangle = None	
+			success_square = False;
+			success_triangle = False;
 			if self.got_camera_info:
 				#Convert ROS image to CV image
 				try:
@@ -112,39 +125,48 @@ class ImageProcessor():
 					return
 
 				# Image mask for colour filtering
-				mask_image = self.process_image(cv_image)
+				mask_image_square = self.process_image(cv_image, 1)
+				#mask_image_triangle = self.process_image(cv_image, 2)
 
 			if cv_image is not None:
 				# ===================
 				# Do processing here!
 				# ===================
 				#gray = cv2.cvtColor(mask_image, cv2.COLOR_HSV2GRAY)
-				gray = mask_image
 
-				sign = self.sign_cascade.detectMultiScale(gray, 1.01, 1, minSize=(100,100))
+				sign_square = self.sign_cascade_square.detectMultiScale(mask_image_square, 1.01, 1, minSize=(25,25))
 
-				for (x,y,w,h) in sign:
-					cv2.rectangle(cv_image,(x,y),(x+w,y+h),(255,0,0),2)
-				# ===================
-
-					self.model_image = np.array([
+				for (x,y,w,h) in sign_square:
+					cv2.rectangle(cv_image,(x,y),(x+w,y+h),(0,165,255),2)
+					self.model_image_square = np.array([
 											(x, y),
 											(x+w, y+h),
 											(x+w, y-h),
 											(x-w, y+h),
-											(x-w, y-h)])
+											(x-w, y-h)], dtype=np.float64)
 
 				# Do the SolvePnP method
-				#(success, rvec, tvec) = cv2.solvePnP(self.model_object, self.model_image, self.camera_matrix, self.dist_coeffs)
+				if self.model_image_square is not None:
+					(success_square, rvec_square, tvec_square) = cv2.solvePnP(self.model_object_square, self.model_image_square, self.camera_matrix, self.dist_coeffs)
+					#print('x transform [cm]')
+					#print(tvec[0]*100)
+					#print('\n')
+					#print('y transform [cm]')
+					#print(tvec[1]*100)
+					#print('\n')
+					#print('z transform [cm]')
+					#print(tvec[2]*100)
+					#print('\n')
 
 				# If a result was found, send to TF2
-				if success:
+
+				if success_square:
 					msg_out = TransformStamped()
 					msg_out.header = msg_in.header
-					msg_out.child_frame_id = "triangle"
-					msg_out.transform.translation.x = tvec[0]
-					msg_out.transform.translation.y = tvec[1]
-					msg_out.transform.translation.z = tvec[2]
+					msg_out.child_frame_id = "Square"
+					msg_out.transform.translation.x = tvec_square[0]
+					msg_out.transform.translation.y = tvec_square[1]
+					msg_out.transform.translation.z = tvec_square[2]
 					msg_out.transform.rotation.w = 1.0	# Could use rvec, but need to convert from DCM to quaternion first
 					msg_out.transform.rotation.x = 0.0
 					msg_out.transform.rotation.y = 0.0
@@ -156,35 +178,37 @@ class ImageProcessor():
 				# Convert CV image to ROS image and publish
 				try:
 					self.pub_overlay.publish( self.bridge.cv2_to_compressed_imgmsg( cv_image ) )
-					self.pub_mask.publish( self.bridge.cv2_to_compressed_imgmsg( gray ) )
+					self.pub_mask.publish( self.bridge.cv2_to_compressed_imgmsg( mask_image_square ) )
 				except CvBridgeError as e:
 					print(e)
 		
 
-	def process_image(self, cv_image):
+	def process_image(self, cv_image, process_type):
 		#Convert the image to HSV and prepare the mask
 		hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 		mask_image = None
 
-		hue_lower = (self.param_hue_center - self.param_hue_range) % 180
-		hue_upper = (self.param_hue_center + self.param_hue_range) % 180
+		if process_type == 1: #Square
 
-		thresh_lower = np.array([hue_lower, self.param_val_min, self.param_val_min])
-		thresh_upper = np.array([hue_upper, self.param_val_max, self.param_val_max])
+			hue_lower_square = (self.param_hue_center_square - self.param_hue_range_square) % 180
+			hue_upper_square = (self.param_hue_center_square + self.param_hue_range_square) % 180
+
+			thresh_lower_square = np.array([hue_lower_square, self.param_val_min_square, self.param_val_min_square])
+			thresh_upper_square = np.array([hue_upper_square, self.param_val_max_square, self.param_val_max_square])
 
 
-		if hue_lower > hue_upper:
-			# We need to do a wrap around HSV 180 to 0 if the user wants to mask this color
-			thresh_lower_wrap = np.array([180, self.param_sat_max, self.param_val_max])
-			thresh_upper_wrap = np.array([0, self.param_sat_min, self.param_val_min])
+			if hue_lower_square > hue_upper_square:
+				# We need to do a wrap around HSV 180 to 0 if the user wants to mask this color
+				thresh_lower_wrap_square = np.array([180, self.param_sat_max_square, self.param_val_max_square])
+				thresh_upper_wrap_square = np.array([0, self.param_sat_min_square, self.param_val_min_square])
 
-			mask_lower = cv2.inRange(hsv_image, thresh_lower, thresh_lower_wrap)
-			mask_upper = cv2.inRange(hsv_image, thresh_upper_wrap, thresh_upper)
+				mask_lower_square = cv2.inRange(hsv_image, thresh_lower_square, thresh_lower_wrap_square)
+				mask_upper_square = cv2.inRange(hsv_image, thresh_upper_wrap_square, thresh_upper_square)
 
-			mask_image = cv2.bitwise_or(mask_lower, mask_upper)
-		else:
-			# Otherwise do a simple mask
-			mask_image = cv2.inRange(hsv_image, thresh_lower, thresh_upper)
+				mask_image = cv2.bitwise_or(mask_lower_square, mask_upper_square)
+			else:
+				# Otherwise do a simple mask
+				mask_image = cv2.inRange(hsv_image, thresh_lower_square, thresh_upper_square)
 
 		# Refine image to get better results
 		kernel = np.ones((5,5),np.uint8)
